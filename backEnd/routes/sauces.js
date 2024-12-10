cce5ff62dc6cf6567b485467e64b9d9d096b0628
xdang <xdangleterre@hotmail.com>
2024-12-10 08:40:56 +0100

    routesSauces + modelSauce

---
+++ backEnd/routes/sauces.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routerSauce = express.Router();
+
+const auth = require('../middleware/authMiddleware');
+const multer = require('../middleware/multerSetting');
+
+
+// manque : le controleur sauce
+
+
+// manque le controle et identification des routes
+
+
+module.exports = routerSauce;
